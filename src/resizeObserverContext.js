5a208f055c15dee980a4b65a1df800f6810493f9
Marc Dingena <marc.dingena@gmail.com>
2019-11-22 23:02:48 +1100

    Create resizeObserverContext.js

---
+++ src/resizeObserverContext.js
@@ -0,0 +1,19 @@
+import { createContext } from 'react';
+
+const ResizeObserverContext = createContext(null);
+
+/**
+ * Bootstraps a new ResizeObserver with a callback function used by the `useResizeObserver` hook.
+ * @param {ResizeObserver} [ResizeObserver] - Optional `ResizeObserver` constructor, for example from a ponyfill. Defaults to `window.ResizeObserver`. CAUTION: https://caniuse.com/#feat=mdn-api_resizeobserver_resizeobserver
+ * @returns {ResizeObserver} Bootstrapped ResizeObserver to assign to `ResizeObserverContext.Provider`'s value.
+ */
+const createResizeObserver = (ResizeObserver = window.ResizeObserver) => {
+  const handleEntry = entry => {
+    const { handleResizeObservation } = entry.target;
+    handleResizeObservation && handleResizeObservation(entry);
+  };
+
+  return new ResizeObserver(entries => entries.forEach(handleEntry));
+};
+
+export { ResizeObserverContext, createResizeObserver };
