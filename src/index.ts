d9947cd40762ed941ca37fb0adc6677291b7bdde
Marc Dingena <marc.dingena@gmail.com>
2020-10-05 16:12:16 +1100

    Do not expose temporary temporary extended interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { ResizeObserverContext, createResizeObserver, Provider } from './ResizeObserverContext';
 export { useResizeObserver } from './useResizeObserver';
-export { ExtendedElement } from './ExtendedElement';
-export { ExtendedResizeObserverEntry } from './ExtendedResizeObserverEntry';
